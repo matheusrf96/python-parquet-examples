69ab6cd7a1b482543cdbd291951f2df524ef0d46
Matheus Fernandes <matheus.fernandes@lend.tech>
2024-12-17 16:11:52 -0300

    refactor: Replace base data file and refactor write_parquet script

--- src/write_parquet.py
+++ src/write_parquet.py
@@ -7,49 +7,52 @@ import pandas as pd
 
 def retrieve_data_from_csv() -> Dict:
     data = {
-        "year": [],
-        "industry_aggregation": [],
-        "industry_code": [],
-        "industry_name": [],
+        "series_id": [],
+        "period": [],
+        "data_value": [],
+        "suppressed": [],
+        "status": [],
         "units": [],
-        "variable_code": [],
-        "variable_name": [],
-        "variable_category": [],
-        "value": [],
+        "magnitude": [],
+        "subject": [],
+        "group": [],
+        "series_title": [],
     }
 
     print("reading csv lines")
-    FILE_PATH = "../files/annual-enterprise-survey-2023-financial-year-provisional.csv"
+    FILE_PATH = "../files/balance-of-payments-and-international-investment-position-june-2024-quarter.csv"  # noqa
     with open(FILE_PATH, newline="") as csvfile:
         spamreader = csv.reader(csvfile, delimiter=",", quotechar="\"")
         for i, row in enumerate(spamreader):
             if i == 0:
                 continue
 
-            data["year"].append(row[0])
-            data["industry_aggregation"].append(row[1])
-            data["industry_code"].append(row[2])
-            data["industry_name"].append(row[3])
-            data["units"].append(row[4])
-            data["variable_code"].append(row[5])
-            data["variable_name"].append(row[6])
-            data["variable_category"].append(row[7])
-            data["value"].append(row[8])
+            data["series_id"].append(row[0])
+            data["period"].append(row[1])
+            data["data_value"].append(row[2] or "0")
+            data["suppressed"].append(row[3])
+            data["status"].append(row[4])
+            data["units"].append(row[5])
+            data["magnitude"].append(row[6])
+            data["subject"].append(row[7])
+            data["group"].append(row[8])
+            data["series_title"].append(row[9])
 
     return data
 
 
-def convert_csv_data_into_pandas_dataframe() -> Any:
+def convert_csv_data_into_pandas_dataframe(data: Dict) -> Any:
     print("converting csv data into pandas dataframe")
-    data = retrieve_data_from_csv()
     return pd.DataFrame(data=data)
 
 
-def convert_dataframe_into_parquet_file() -> None:
+def convert_dataframe_into_parquet_file(data: Any) -> None:
+    FILE_PATH = "../files/example.parquet"
     print("creating parquet file")
-    data = convert_csv_data_into_pandas_dataframe()
-    data.to_parquet("../files/example.parquet", index=False)
+    data.to_parquet(FILE_PATH, index=False)
     print("parquet file has been created successfully")
 
 
-convert_dataframe_into_parquet_file()
+csv_data = retrieve_data_from_csv()
+dataframe = convert_csv_data_into_pandas_dataframe(data=csv_data)
+convert_dataframe_into_parquet_file(data=dataframe)
