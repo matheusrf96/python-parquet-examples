3b1c65ad2ea427fa443de832f257db3cc53edb9c
Matheus Fernandes <matheus.fernandes@lend.tech>
2024-12-18 15:01:27 -0300

    feat: Create script to filter "Estabelecimentos1" parquet file

---
+++ src/filter_parquet_estabelecimentos.py
@@ -0,0 +1,15 @@
+import pyarrow.parquet as pq
+
+# Filters: List of tuples
+# Filter Tuple (key, op, value): pos[0] == "column name", pos[1] == "operator", pos[2] == "value"  # noqa
+filters = [
+    ("cnpj_1", "==", "64904295"),
+]
+
+FILE_PATH = "../files/estabelecimentos/Estabelecimentos1.parq"
+table = pq.read_table(FILE_PATH, filters=filters)
+filtered_df = table.to_pandas()
+
+print(filtered_df)
+print()
+print(filtered_df.to_dict("records"))
